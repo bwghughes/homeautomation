21150ba79475d0c44504f66b62d5013b60a7b9d7
Ben Hughes <bwghughes@gmail.com>
2014-12-27 17:29:26 +0000

    ksjld

--- app.py
+++ app.py
@@ -6,15 +6,20 @@ from energenie import switch_off, switch_on
 app = Flask(__name__)
 api = restful.Api(app)
 
+states = {1: 'Off'}
+
 class Lamp(restful.Resource):
     def get(self, lamp_id):
-        return {'lamp_id': lamp_id}
+        return {'lamp_id': states.get(lamp_id)}
 
     def put(self, lamp_id):
-        lamp_id = request.form['data'].get('lamp_id')
-        return {'status': 'OK'}
+        lamp_id = int(request.form['data'].get('lamp_id'))
+        if states.get(lamp_id) == 'Off':
+            states[lamp_id] == 'On'
+            switch_on(lamp_id)
+        return states
 
 api.add_resource(Lamp, '/lamp/<string:lamp_id>')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run("0.0.0.0", debug=True)
