6c328fb0a39473ba18bfa66b29a227dc9998e06c
Ben Hughes <bwghughes@gmail.com>
2014-12-27 20:22:07 +0000

    lk

--- job.py
+++ job.py
@@ -5,8 +5,9 @@ from astral import Astral
 from energenie import switch_off, switch_on
 
 import logging
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(filname='tasks.log', level=logging.INFO)
 logger = logging.getLogger(__name__)
+
 
 def is_light_outside(dawn, dusk, now=datetime.datetime.now()):
 	now = pytz.utc.localize(now)
@@ -21,8 +22,10 @@ def main():
 	timezone = city.timezone
 	sun = city.sun(date=datetime.date.today(), local=True)
 	if is_light_outside(sun['dawn'], sun['dusk']):
+		logger.info("Its light outside, switching off...")
 		switch_off(1)
 	else:
+		logger.info("Its dark outside, switching on...")
 		switch_on(1)
 
 
