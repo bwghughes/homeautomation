3f88bd9d962e6bba55b9d5ea3d308d9e51f688d4
Ben Hughes <bwghughes@gmail.com>
2014-12-30 12:32:37 +0000

    use-gunicorn-properly

--- fabfile.py
+++ fabfile.py
@@ -14,7 +14,10 @@ def qd(comment):
     local("git add . && git commit -m \"{}\" && git push origin master".format(comment))
     with cd ('/home/pi/apps/homeautomation/'):
         run('git pull')
-        sudo('/etc/init.d/supervisor restart')
+
+@task
+def restart():
+    sudo('/etc/init.d/supervisor restart')
 
 
 @task
